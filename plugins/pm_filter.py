2cfc592698157016cb4115691ca75c7740def563
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2022-01-27 16:49:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -620,7 +620,7 @@ async def auto_filter(client, msg, spoll=False):
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
     pre = 'filep' if settings['file_secure'] else 'file'
-    btn.insert([InlineKeyboardButton("ɪɴꜰᴏ", "callback1"), InlineKeyboardButton("ᴍᴏᴠɪᴇ", "callback2"), InlineKeyboardButton("sᴇʀɪᴇs", "callback3")])
+    buttons.insert([InlineKeyboardButton("ɪɴꜰᴏ", "callback1"), InlineKeyboardButton("ᴍᴏᴠɪᴇ", "callback2"), InlineKeyboardButton("sᴇʀɪᴇs", "callback3")])
     for file in files:
         btn = ([InlineKeyboardButton(text=f"▫ {get_size(file.file_size)} ▸ {file.file_name}", callback_data=f'files#{file.file_id}')])
 
