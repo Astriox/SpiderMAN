dace456b3a0ba54f94ece97d4c5dc24c713f4ab0
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2022-02-11 23:08:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,6 +17,7 @@ import base64
 logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
+ADMIN = ADMINS
 
 @Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
 async def start(client, message):
@@ -286,6 +287,53 @@ async def log_file(bot, message):
         await message.reply_document('TelegramBot.log')
     except Exception as e:
         await message.reply(str(e))
+        
+@Client.on_message(filters.command('submit') & filters.private)
+async def report(bot, message):
+        if message.reply_to_message:
+                                  await bot.send_message(chat_id=ADMIN, text=f"<b>⭕️NEW MESSAGE⭕️\n \n🧿 Name: {message.from_user.mention}\n🧿 User ID:</b> <code>{message.chat.id}</code>")
+                                  await bot.forward_messages(chat_id=ADMIN, from_chat_id=message.from_user.id, message_ids=message.reply_to_message.message_id)
+                                  await message.reply_text("<b>✅ Your Feedback Successfully Submitted to the Admins</b>")
+        else:
+             await message.reply_text("<b>Use this command as the reply of any Message to Report</b>")
+
+@Client.on_message(filters.command('reply') & filters.private)
+async def replyt(bot, message):
+    if message.from_user.id == ADMIN: 
+               if message.reply_to_message:
+                                    userid=int(message.text.replace("/reply"," "))
+                                    await bot.send_message(chat_id=userid, text=f"<b>An Admin is responded to your feedback ✨</b>")
+                                    await bot.copy_message(chat_id=userid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
+                                    await message.reply_text("<b>✅ Your Reply Successfully Send to the User</b>")
+               else:
+                    await message.reply_text("<b>Use this command as the reply of any Message to Reply</b>")                         
+    else:
+         await message.reply_text("<b>That's not for you bruh 😅</b>")
+
+@Client.on_message(filters.command('send') & filters.private)
+async def send(bot, message):
+    if message.from_user.id == ADMIN: 
+               if message.reply_to_message:
+                                    chatid=int(message.text.replace("/send"," "))
+                                    await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
+                                    await message.reply_text("<b>✅ Message Successfully Send to the Group</b>")
+               else:
+                    await message.reply_text("<b>Use this command as the reply of any Message to Send in Group</b>")                         
+    else:
+         await message.reply_text("<b>That's not for you bruh 😅</b>")
+
+@Client.on_message(filters.command('pin') & filters.private)
+async def pin(bot, message):
+    if message.from_user.id == ADMIN: 
+               if message.reply_to_message:
+                                    chatid=int(message.text.replace("/pin"," "))
+                                    p=await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
+                                    await p.pin()
+                                    await message.reply_text("<b>✅ Message Successfully Send to the Group And pinned</b>")
+               else:
+                    await message.reply_text("<b>Use this command as the reply of any Message to Send in Group</b>")                         
+    else:
+         await message.reply_text("<b>That's not for you bruh 😅</b>")
 
 @Client.on_message(filters.command('delete') & filters.user(ADMINS))
 async def delete(bot, message):
