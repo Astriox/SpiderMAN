8e6d7cc4851b832281ab97aa2c1b5c7545a1caa5
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2022-02-11 21:58:35 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -127,6 +127,30 @@ async def who_is(client, message):
         )
     await status_message.delete()
 
+@Client.on_message(filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
+async def report(bot, message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        reporter = str(message.from_user.id)
+        mention = message.from_user.mention
+        admins = await bot.get_chat_members(chat_id=chat_id, filter="administrators")
+        success = False
+        report = f"Reporter : {mention} ({reporter})" + "\n"
+        report += f"Message : {message.reply_to_message.link}"
+        for admin in admins:
+            try:
+                reported_post = await message.reply_to_message.forward(admin.user.id)
+                await reported_post.reply_text(
+                    text=report,
+                    chat_id=admin.user.id,
+                    disable_web_page_preview=True
+                )
+                success = True
+            except:
+                pass
+        if success:
+            await message.reply_text("**Reported to Admins!**")
+            
 @Client.on_message(filters.command(["imdb", 'search']) & filters.user(ADMINS))
 async def imdb_search(client, message):
     if ' ' in message.text:
