f8a99e8957ef2a8c6c6f60c877e4e674ea573887
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2022-03-19 11:07:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -443,7 +443,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data='close_pages')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
+        w=await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
@@ -454,7 +454,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
+        await w.edit_photo(
+            photo='https://telegra.ph/file/a3beeb61ced5563655832.jpg',
             text=script.OWNER_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
