50ae1418d60e45e3082a4c37296c7202a3596a17
ᴀsᴛʀɪᴏx <71863236+Astriox@users.noreply.github.com>
2023-01-14 08:57:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -185,7 +185,7 @@ async def start(client, message):
         return await sts.delete()
         
 
-files_ = await get_file_details(file_id)           
+    files_ = await get_file_details(file_id)           
     if not files_:
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
