c4f3ec9c36cfa971dda1cc1e67129b58769908b8
ᴀsᴛʀɪᴏx <71863236+Astriox@users.noreply.github.com>
2023-04-05 00:43:40 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,360 @@
+class script(object):
+    START_TXT = """<b>Hᴇʟʟᴏ {}</b>
+    
+<b>Sᴏʀʀʏ ɪ ᴏɴʟʏ ᴡᴏʀᴋ ᴏɴ</b> <b><a href=https://t.me/Cinimabranthen>Cɪɴɪᴍᴀʙʀᴀɴᴛʜᴇɴ</a></b><b> Gʀᴏᴜᴘ. Nᴏ ᴏᴛʜᴇʀ ᴄᴏᴍᴍᴀɴᴅ ᴡɪʟʟ ᴡᴏʀᴋ ᴏɴ ᴛʜɪs ʙᴏᴛ ᴇxᴄᴇᴘᴛ sᴛᴀʀᴛ. ᴅᴏɴ’ᴛ ᴡᴀsᴛᴇ ʏᴏᴜʀ ᴛɪᴍᴇ</b>"""
+    OWNER_TXT = """<b>⍟───[ ᴏᴡɴᴇʀ ᴅᴇᴛᴀɪʟꜱ ]───⍟</b>
+    
+<b>• ꜰᴜʟʟ ɴᴀᴍᴇ : ᴘⲁ₪ᴄʜᴏ</b>
+<b>• Uꜱᴇʀ ɴᴀᴍᴇ : @iampancho
+• Usᴇʀ ɪᴅ :</b> <code>1700221515</code>
+    
+<b>∂σи'т вє αfяαι∂ σf вєιиg ∂ιffєяєит, вє αfяαι∂ σf вєιиg тнє ѕαмє αѕ єνєяуσиє єℓѕє</b>
+    """
+
+
+# 鈿狅笍 Please don't change our source code 馃憞馃徎
+
+    ABOUT_TXT = """<b>鉁� M蕪 N岽�岽嶀磭: {}
+鉁� C蕗岽囜磤岽涐磸蕗: <a href='tg://settings'>T蕼瑟s 岽樶磭蕗s岽徤�</a>
+鉁� L瑟蕶蕗岽�蕗蕪: <a href='https://docs.pyrogram.org/'>P蕪蕗岽徤⑹�岽�岽�</a>
+鉁� D岽囜礌岽徥熱磸岽樶磭蕗: <a href='https://t.me/AFxSU'>饾悅饾惈饾悮饾惓饾惒 饾悆饾悶饾惎饾悶饾惀饾惃饾惄饾悶饾惈</a>
+鉁� L岽�纱散岽溼磤散岽�: <a href='https://www.python.org/download/releases/3.0/'>P蕪岽浭溼磸纱 3</a>
+鉁� D岽�岽涐磤B岽�s岽�: <a href='https://www.mongodb.com/'>M岽徤瓷⑨磸DB</a>
+鉁� B岽忈礇 S岽囀�岽犪磭蕗: <a href='https://t.me/quickfastt'>Q岽溕磩岽� F岽�s岽�</a>
+鉁� B岽溕熱磪 S岽涐磤岽涐礈s: v2.0.3 [ S岽涐磤蕶薀岽� ]</b>"""
+
+    SOURCE_TXT = """<b>NOTE:</b>
+<b>- 饾殐饾櫡饾櫢饾殏 饾櫢饾殏 饾櫚饾櫧 饾櫢饾殏 饾櫨饾櫩饾櫞饾櫧 饾殏饾櫨饾殑饾殎饾櫜饾櫞 饾櫩饾殎饾櫨饾櫣饾櫞饾櫜饾殐. 
+- 饾殏饾櫨饾殑饾殎饾櫜饾櫞 - 饾櫜饾櫥饾櫢饾櫜饾櫤 饾殎饾櫞饾櫩饾櫨 饾櫛饾殑饾殐饾殐饾櫨饾櫧</b>
+<b>DEVS:</b>
+- <a href=https://t.me/czdbotz>饾櫜饾殙饾櫝 饾櫛饾櫨饾殐饾殙</a>"""
+
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
+
+<b>- 饾櫟饾櫢饾櫥饾殐饾櫞饾殎 饾櫢饾殏 饾殐饾櫡饾櫞 饾櫟饾櫞饾櫚饾殐饾殑饾殎饾櫞 饾殕饾櫞饾殎饾櫞 饾殑饾殏饾櫞饾殎饾殏 饾櫜饾櫚饾櫧 饾殏饾櫞饾殐 饾櫚饾殑饾殐饾櫨饾櫦饾櫚饾殐饾櫞饾櫝 饾殎饾櫞饾櫩饾櫥饾櫢饾櫞饾殏 饾櫟饾櫨饾殎 饾櫚 饾櫩饾櫚饾殎饾殐饾櫢饾櫜饾殑饾櫥饾櫚饾殎 饾櫤饾櫞饾殘饾殕饾櫨饾殎饾櫝 饾櫚饾櫧饾櫝 饾櫞饾櫥饾殏饾櫚 饾殕饾櫢饾櫥饾櫥 饾殎饾櫞饾殏饾櫩饾櫨饾櫧饾櫝 饾殕饾櫡饾櫞饾櫧饾櫞饾殔饾櫞饾殎 饾櫚 饾櫤饾櫞饾殘饾殕饾櫨饾殎饾櫝 饾櫢饾殏 饾櫟饾櫨饾殑饾櫧饾櫝 饾殐饾櫡饾櫞 饾櫦饾櫞饾殏饾殏饾櫚饾櫠饾櫞</b>
+
+<b>NOTE:</b>
+<b>饾煼. 饾櫝饾櫨饾櫜饾殐饾櫨饾殎-饾殏饾殐饾殎饾櫚饾櫧饾櫠饾櫞 饾殏饾櫡饾櫨饾殑饾櫥饾櫝 饾櫡饾櫚饾殔饾櫞 饾櫚饾櫝饾櫦饾櫢饾櫧 饾櫩饾殎饾櫢饾殔饾櫢饾櫥饾櫥饾櫚饾櫠饾櫞.
+饾煾. 饾櫨饾櫧饾櫥饾殘 饾櫚饾櫝饾櫦饾櫢饾櫧饾殏 饾櫜饾櫚饾櫧 饾櫚饾櫝饾櫝 饾櫟饾櫢饾櫥饾殐饾櫞饾殎饾殏 饾櫢饾櫧 饾櫚 饾櫜饾櫡饾櫚饾殐.
+饾煿. 饾櫚饾櫥饾櫞饾殎饾殐 饾櫛饾殑饾殐饾殐饾櫨饾櫧饾殏 饾櫡饾櫚饾殔饾櫞 饾櫚 饾櫥饾櫢饾櫦饾櫢饾殐 饾櫨饾櫟 饾熂饾熀 饾櫜饾櫡饾櫚饾殎饾櫚饾櫜饾殐饾櫞饾殎饾殏.</b>
+
+<b>Commands and Usage:</b>
+鈥� /filter - <code>add a filter in chat</code>
+鈥� /filters - <code>list all the filters of a chat</code>
+鈥� /del - <code>delete a specific filter in chat</code>
+鈥� /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+
+    BUTTON_TXT = """Help: <b>Buttons</b>
+
+<b>饾櫝饾櫨饾櫜饾殐饾櫨饾殎-饾殏饾殐饾殎饾櫚饾櫧饾櫠饾櫞 饾殏饾殑饾櫩饾櫩饾櫨饾殎饾殐饾殏 饾櫛饾櫨饾殐饾櫡 饾殑饾殎饾櫥 饾櫚饾櫧饾櫝 饾櫚饾櫥饾櫞饾殎饾殐 饾櫢饾櫧饾櫥饾櫢饾櫧饾櫞 饾櫛饾殑饾殐饾殐饾櫨饾櫧饾殏.</b>
+<b>NOTE:</b>
+<b>饾煼. 饾殐饾櫞饾櫥饾櫞饾櫠饾殎饾櫚饾櫦 饾殕饾櫢饾櫥饾櫥 饾櫧饾櫨饾殐 饾櫚饾櫥饾櫥饾櫨饾殕饾殏 饾殘饾櫨饾殑 饾殐饾櫨 饾殏饾櫞饾櫧饾櫝 饾櫛饾殑饾殐饾殐饾櫨饾櫧饾殏 饾殕饾櫢饾殐饾櫡饾櫨饾殑饾殐 饾櫚饾櫧饾殘 饾櫜饾櫨饾櫧饾殐饾櫞饾櫧饾殐, 饾殏饾櫨 饾櫜饾櫨饾櫧饾殐饾櫞饾櫧饾殐 饾櫢饾殏 饾櫦饾櫚饾櫧饾櫝饾櫚饾殐饾櫨饾殎饾殘.
+饾煾. 饾櫝饾櫨饾櫜饾殐饾櫨饾殎-饾殏饾殐饾殎饾櫚饾櫧饾櫠饾櫞 饾殏饾殑饾櫩饾櫩饾櫨饾殎饾殐饾殏 饾櫛饾殑饾殐饾殐饾櫨饾櫧饾殏 饾殕饾櫢饾殐饾櫡 饾櫚饾櫧饾殘 饾殐饾櫞饾櫥饾櫞饾櫠饾殎饾櫚饾櫦 饾櫦饾櫞饾櫝饾櫢饾櫚 饾殐饾殘饾櫩饾櫞.
+饾煿. 饾櫛饾殑饾殐饾殐饾櫨饾櫧饾殏 饾殏饾櫡饾櫨饾殑饾櫥饾櫝 饾櫛饾櫞 饾櫩饾殎饾櫨饾櫩饾櫞饾殎饾櫥饾殘 饾櫩饾櫚饾殎饾殏饾櫞饾櫝 饾櫚饾殏 饾櫦饾櫚饾殎饾櫤饾櫝饾櫨饾殕饾櫧 饾櫟饾櫨饾殎饾櫦饾櫚饾殐</b>
+<b>URL buttons:</b>
+<code>[Button Text](buttonurl:https://t.me/Example...)</code>
+
+<b>Alert buttons:</b>
+<code>[Button Text](buttonalert:This is an alert message)</code>"""
+
+    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+
+<b>NOTE:</b>
+<b>饾煼. 饾櫦饾櫚饾櫤饾櫞 饾櫦饾櫞 饾殐饾櫡饾櫞 饾櫚饾櫝饾櫦饾櫢饾櫧 饾櫨饾櫟 饾殘饾櫨饾殑饾殎 饾櫜饾櫡饾櫚饾櫧饾櫧饾櫞饾櫥 饾櫢饾櫟 饾櫢饾殐'饾殏 饾櫩饾殎饾櫢饾殔饾櫚饾殐饾櫞.
+饾煾. 饾櫦饾櫚饾櫤饾櫞 饾殏饾殑饾殎饾櫞 饾殐饾櫡饾櫚饾殐 饾殘饾櫨饾殑饾殎 饾櫜饾櫡饾櫚饾櫧饾櫧饾櫞饾櫥 饾櫝饾櫨饾櫞饾殏 饾櫧饾櫨饾殐 饾櫜饾櫨饾櫧饾殐饾櫚饾櫢饾櫧饾殏 饾櫜饾櫚饾櫦饾殎饾櫢饾櫩饾殏, 饾櫩饾櫨饾殎饾櫧 饾櫚饾櫧饾櫝 饾櫟饾櫚饾櫤饾櫞 饾櫟饾櫢饾櫥饾櫞饾殏.
+饾煿. 饾櫟饾櫨饾殎饾殕饾櫚饾殎饾櫝 饾殐饾櫡饾櫞 饾櫥饾櫚饾殏饾殐 饾櫦饾櫞饾殏饾殏饾櫚饾櫠饾櫞 饾殐饾櫨 饾櫦饾櫞 饾殕饾櫢饾殐饾櫡 饾殌饾殑饾櫨饾殐饾櫞饾殏.
+ 饾櫢'饾櫥饾櫥 饾櫚饾櫝饾櫝 饾櫚饾櫥饾櫥 饾殐饾櫡饾櫞 饾櫟饾櫢饾櫥饾櫞饾殏 饾櫢饾櫧 饾殐饾櫡饾櫚饾殐 饾櫜饾櫡饾櫚饾櫧饾櫧饾櫞饾櫥 饾殐饾櫨 饾櫦饾殘 饾櫝饾櫛.</b>
+<b>鈽� /set_template - 饾殏饾櫞饾殐 饾櫜饾殑饾殏饾殐饾櫨饾櫦 饾櫢饾櫦饾櫝饾櫛 饾殐饾櫞饾櫦饾櫩饾櫥饾櫚饾殐饾櫞 饾櫟饾櫨饾殎 饾櫚饾殑饾殐饾櫨 饾櫟饾櫢饾櫥饾殐饾櫞饾殎.</b>
+<b>鈽� /get_template - 饾櫠饾櫞饾殐 饾櫜饾殑饾殎饾殎饾櫞饾櫧饾殐 饾櫢饾櫦饾櫝饾櫛 饾殐饾櫞饾櫦饾櫩饾櫥饾櫚饾殐饾櫞 饾櫨饾櫟 饾櫚饾殑饾殐饾櫨 饾櫟饾櫢饾櫥饾殐饾櫞饾殎.</b>
+<b>鈽� /autofilter on - 饾櫞饾櫧饾櫚饾櫛饾櫥饾櫞 饾櫚饾殑饾殐饾櫨 饾櫟饾櫢饾櫥饾殐饾櫞饾殎 饾櫢饾櫧 饾殐饾櫡饾櫞 饾櫠饾殎饾櫨饾殑饾櫩饾殏.</b>
+<b>鈽� /autofilter off - 饾櫝饾櫢饾殏饾櫚饾櫛饾櫥饾櫞饾櫝 饾櫚饾殑饾殐饾櫨 饾櫟饾櫢饾櫥饾殐饾櫞饾殎 饾櫢饾櫧 饾殐饾櫡饾櫞 饾櫠饾殎饾櫨饾殑饾櫩饾殏.</b>"""
+
+    CONNECTION_TXT = """Help: <b>Connections</b>
+
+- Used to connect bot to PM for managing filters 
+- it helps to avoid spamming in groups.
+
+<b>NOTE:</b>
+1. Only admins can add a connection.
+2. Send <code>/connect</code> for connecting me to ur PM
+
+<b>Commands and Usage:</b>
+鈥� /connect  - <code>connect a particular chat to your PM</code>
+鈥� /disconnect  - <code>disconnect from a chat</code>
+鈥� /connections - <code>list all your connections</code>"""
+
+    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
+
+<b>NOTE:</b>
+these are the extra features of Elsa
+
+<b>Commands and Usage:</b>
+鈥� /id - <code>get id of a specified user.</code>
+鈥� /info  - <code>get information about a user.</code>
+鈥� /imdb  - <code>get the film information from IMDb source.</code>
+鈥� /search  - <code>get the film information from various sources.</code>"""
+
+    SONG_TXT = """<b>饾殏饾櫨饾櫧饾櫠 饾櫝饾櫨饾殕饾櫧饾櫥饾櫨饾櫚饾櫝 饾櫦饾櫨饾櫝饾殑饾櫥饾櫞</b>
+
+<i>饾殏饾櫨饾櫧饾櫠 饾櫝饾櫨饾殕饾櫧饾櫥饾櫨饾櫚饾櫝 饾櫦饾櫨饾櫝饾殑饾櫥饾櫞, 饾櫟饾櫨饾殎 饾殐饾櫡饾櫨饾殏饾櫞 饾殕饾櫡饾櫨 饾櫥饾櫨饾殔饾櫞 饾櫦饾殑饾殏饾櫢饾櫜. 饾殘饾櫨饾殑 饾櫜饾櫚饾櫧 饾殑饾殏饾櫞 饾殐饾櫡饾櫢饾殏 饾櫟饾櫞饾櫚饾殐饾殑饾櫞 饾櫟饾櫨饾殎 饾櫝饾櫨饾殕饾櫧饾櫥饾櫨饾櫚饾櫝 饾櫚饾櫧饾殘 饾殏饾櫨饾櫧饾櫠 饾殕饾櫢饾殐饾櫡 饾殏饾殑饾櫩饾櫞饾殎 饾櫟饾櫚饾殏饾殐 饾殏饾櫩饾櫞饾櫞饾櫝.饾殕饾櫨饾殎饾櫤饾殏 饾櫨饾櫧饾櫥饾殘 饾櫨饾櫧 饾櫠饾殎饾櫨饾殑饾櫩饾殏../</i>
+
+<b>饾櫜饾櫨饾櫦饾櫦饾櫚饾櫧饾櫝饾殏</b>
+
+鈴笍 /song 饾殏饾櫨饾櫧饾櫠 饾櫧饾櫚饾櫦饾櫞 
+
+<b>饾殕饾櫨饾殎饾櫤饾殏 饾櫛饾櫨饾殐饾櫡 饾櫠饾殎饾櫨饾殑饾櫩 饾櫚饾櫧饾櫝 饾櫩饾櫦</b>
+@at3movies"""
+
+    VIDEO_TXT ="""饾櫡饾櫞饾櫥饾櫩 饾殘饾櫨饾殑 饾殐饾櫨 饾櫝饾櫨饾殕饾櫧饾櫥饾櫨饾櫚饾櫝 饾殔饾櫢饾櫝饾櫞饾櫨 饾櫟饾殎饾櫨饾櫦 饾殘饾櫨饾殑饾殐饾殑饾櫛饾櫞.
+鈥� 饾槣饾槾饾槩饾槰饾槮
+饾槧饾槹饾樁 饾槉饾槩饾槸 饾構饾槹饾樃饾槸饾槶饾槹饾槩饾槬 饾槇饾槸饾樅 饾槤饾槳饾槬饾槮饾槹 饾槏饾槼饾槹饾槷 饾槧饾槹饾樁饾樀饾樁饾槪饾槮
+饾檭饾櫎饾櫖 饾檹饾櫎 饾檺饾櫒饾櫄
+鈥� 饾槢饾樅饾槺饾槮 /video or /mp4 饾槇饾槸饾槬 (https://youtu.be/example...)
+鈥� 饾槍饾樄饾槩饾槷饾槺饾槶饾槮:
+<code>/mp4 https://youtu.be/example...</code>
+<code>/video https://youtu.be/example...</code>"""
+
+    TTS_TXT = """Help: <b> TTS 馃帳 module:</b>
+Translate text to speech
+<b>Commands and Usage:</b>
+鈥� /tts <text> : convert text to speech"""
+
+    GTRANS_TXT = """鉃� 饾悋饾悶饾惀饾惄: 饾枽饾棃饾棃饾梹饾梾饾柧 饾柍饾棆饾柡饾棁饾棇饾梾饾柡饾棈饾柧饾棆
+饾殐饾殤饾殥饾殰 饾殞饾殬饾殩饾殩饾殜饾殫饾殟 饾殤饾殠饾殨饾殭饾殰 饾殺饾殬饾殲 饾殱饾殬 饾殱饾殯饾殜饾殫饾殰饾殨饾殜饾殱饾殠 饾殜 饾殱饾殠饾殹饾殱 饾殱饾殬 饾柡饾棁饾棐 饾殨饾殜饾殫饾殣饾殲饾殜饾殣饾殠饾殰 饾殺饾殬饾殲 饾殸饾殜饾殫饾殱. 饾殐饾殤饾殥饾殰 饾殞饾殬饾殩饾殩饾殜饾殫饾殟 饾殸饾殬饾殯饾殧饾殰 饾殬饾殫 饾殝饾殬饾殱饾殤 饾殭饾殩 饾殜饾殫饾殟 饾殣饾殯饾殬饾殲饾殭 鉁�
+鉃� 饾悅饾惃饾惁饾惁饾悮饾惂饾悵饾惉 饾悮饾惂饾悵 饾悢饾惉饾悮饾悹饾悶:
+鉃�/tr - 饾柍饾棃 饾棈饾棆饾柡饾棁饾棇饾梾饾柡饾棈饾柧饾棆 饾棈饾柧饾棏饾棈饾棇 饾棈饾棃 饾柡 饾棇饾棄饾柧饾柤饾梻饾柨饾柤 饾梾饾柡饾棁饾梹饾棊饾柡饾梹饾柧
+鉃� 饾柇饾棃饾棈饾柧:
+饾柖饾梺饾梻饾梾饾柧 饾棊饾棇饾梻饾棁饾梹 /tr 饾棐饾棃饾棊 饾棇饾梺饾棃饾棊饾梾饾柦 饾棇饾棄饾柧饾柤饾梻饾柨饾棐 饾棈饾梺饾柧 饾梾饾柡饾棁饾梹饾棊饾柡饾梹饾柧 饾柤饾棃饾柦饾柧
+鉃涴潠ゐ潡戰潠吼潡嗮潡夝潡咅潠�: /饾棈饾棆 饾梿饾梾
+鈥� 饾柧饾棁 = 饾枻饾棁饾梹饾梾饾梻饾棇饾梺
+鈥� 饾梿饾梾 = 饾柆饾柡饾梾饾柡饾棐饾柡饾梾饾柡饾梿
+鈥� 饾梺饾梻 = 饾枾饾梻饾棁饾柦饾梻"""
+
+    TELE_TXT = """<b>鈻笍HELP: Telegraph鈻笍</b>
+Do as you wish with telegra.ph module!
+</b>USAGE:</b>
+鉁掞笍 /telegraph - Send me Picture or Vide Under (5MB)
+<b>NOTE:</b>
+鈥� This Command Is Available in goups and pms
+鈥� This Command Can be used by everyone"""
+
+    CORONA_TXT = """鉃� 饾悋饾悶饾惀饾惄: 饾枹饾棃饾棌饾梻饾柦
+饾殐饾殤饾殥饾殰 饾櫜饾殬饾殩饾殩饾殜饾殫饾殟 饾殤饾殠饾殨饾殭饾殰 饾殺饾殬饾殲 饾殱饾殬 饾殧饾殫饾殬饾殸 饾殟饾殜饾殥饾殨饾殺 饾殥饾殫饾殢饾殬饾殯饾殩饾殜饾殱饾殥饾殬饾殫 饾殜饾殝饾殬饾殲饾殱 饾殞饾殬饾殶饾殥饾殟 
+鉃� 饾悅饾惃饾惁饾惁饾悮饾惂饾悵饾惉 饾悮饾惂饾悵 饾悢饾惉饾悮饾悹饾悶:
+鉃� /covid - 饾棊饾棇饾柧 饾棈饾梺饾梻饾棇 饾柤饾棃饾梿饾梿饾柡饾棁饾柦 饾棎饾梻饾棈饾梺 饾棐饾棃饾棊饾棆 饾柤饾棃饾棊饾棁饾棈饾棆饾棐 饾棁饾柡饾梿饾柧 饾棈饾棃 饾梹饾柧饾棈 饾柤饾棃饾棌饾梻饾柦饾柧 饾梻饾棁饾柨饾棃饾棆饾梿饾柡饾棈饾梻饾棃饾棁
+鉃涴潠ゐ潡戰潠吼潡嗮潡夝潡咅潠�:
+<code>/covid 饾枿饾棁饾柦饾梻饾柡</code>
+
+鈿狅笍 Here is a bug. We will fix soon"""
+
+    ABOOK_TXT = """鉃� 饾悋饾悶饾惀饾惄: 饾枲饾棊饾柦饾梻饾棃饾柣饾棃饾棃饾梽
+饾殘饾殬饾殲 饾殞饾殜饾殫 饾殞饾殬饾殫饾殶饾殠饾殯饾殱 饾殜 饾櫩饾櫝饾櫟 饾殢饾殥饾殨饾殠 饾殱饾殬 饾殜 饾殜饾殲饾殟饾殥饾殬 饾殢饾殥饾殨饾殠 饾殸饾殥饾殱饾殤 饾殱饾殤饾殥饾殰 饾殞饾殬饾殩饾殩饾殜饾殫饾殟 鉁�
+鉃� 饾悅饾惃饾惁饾惁饾悮饾惂饾悵饾惉 饾悮饾惂饾悵 饾悢饾惉饾悮饾悹饾悶:
+鉃� /audiobook: 饾柋饾柧饾棄饾梾饾棐 饾棈饾梺饾梻饾棇 饾柤饾棃饾梿饾梿饾柡饾棁饾柦 饾棈饾棃 饾柡饾棁饾棐 饾柉饾枺饾枼 饾棈饾棃 饾梹饾柧饾棁饾柧饾棆饾柡饾棈饾柧 饾棈饾梺饾柧 饾柡饾棊饾柦饾梻饾棃"""
+
+    DEPLOY_TXT= """饾櫢饾櫟 饾殘饾櫨饾殑 饾櫟饾櫚饾櫜饾櫢饾櫧饾櫠 饾櫚饾櫧饾殘 饾櫢饾殏饾殏饾殑饾櫞 饾櫢饾櫧 饾殐饾櫡饾櫞 饾殎饾櫞饾櫩饾櫨 饾櫜饾櫨饾櫧饾殐饾櫚饾櫜饾殐 饾櫦饾櫞..."""
+   
+    PINGS_TXT = """<b>Ping Testing:</b>
+Helps you to know your ping 馃毝馃徏鈥嶁檪锔�
+<b>Commands:</b>
+鈥� /alive - To check you are alive.
+鈥� /help - To get help.
+
+鈥� /ping - <b>To get your ping.</b>
+
+<b>馃洜锔廢sage馃洜锔� :</b>
+鈥� This commands can be used in pm and groups
+鈥� This commands can be used buy everyone in the groups and bots pm
+鈥� Share us for more features"""
+ 
+    STICKER_TXT = """<b>饾殘饾櫨饾殑 饾櫜饾櫚饾櫧 饾殑饾殏饾櫞 饾殐饾櫡饾櫢饾殏 饾櫦饾櫨饾櫝饾殑饾櫥饾櫞 饾殐饾櫨 饾櫟饾櫢饾櫧饾櫝 饾櫚饾櫧饾殘 饾殏饾殐饾櫢饾櫜饾櫤饾櫞饾殎饾殏 饾櫢饾櫝.</b>
+鈥� 饾悢饾悞饾悁饾悊饾悇
+To Get Sticker ID
+ 
+  猸� 饾檭饾櫎饾櫖 饾檹饾櫎 饾檺饾櫒饾櫄
+ 
+鈼� Reply To Any Sticker [/stickerid]"""
+
+    FONT_TXT= """鈿欙笍 饾悢饾悞饾悁饾悊饾悇
+
+饾悩饾悗饾悢 饾悅饾悁饾悕 饾悢饾悞饾悇 饾悡饾悋饾悎饾悞 饾悓饾悗饾悆饾悢饾悑饾悇 饾悡饾悗 饾悅饾悋饾悁饾悕饾悊饾悇 饾悈饾悗饾悕饾悡 饾悞饾悡饾悩饾悑饾悇 
+
+<b>COMMAND</b> : /font your text (optional)
+        <b> Eg:- /font Hello</b>
+
+ <i>This feature added by @饾櫜饾殙饾櫝 饾櫛饾櫨饾殐饾殙"""
+    JSON_TXT = """<b>JSON:</b>
+Bot returns json for all replied messages with /json or /js
+<b>Features:</b>
+Message Editting JSON
+Pm Support
+Group Support
+<b>Note:</b>
+<b>Everyone can use this command , if spaming happens bot will automatically ban you from the group.</b>"""
+
+    WHOIS_TXT ="""<b>WHOIS MODULE</b>
+Note:- <b>Give a user details</b>
+
+鈥�/whois :-give a user full details 馃搼"""
+
+    URLSHORT_TXT = """鉃� 饾悋饾悶饾惀饾惄: 饾柎饾棆饾梾 饾棇饾梺饾棃饾棆饾棈饾棁饾柧饾棆
+<i><b>饾殐饾殤饾殥饾殰 饾殞饾殬饾殩饾殩饾殜饾殫饾殟 饾殤饾殠饾殨饾殭饾殰 饾殺饾殬饾殲 饾殱饾殬 饾殰饾殤饾殬饾殯饾殱 饾殜 饾殲饾殯饾殨 </i></b>
+鉃� 饾悅饾惃饾惁饾惁饾悮饾惂饾悵饾惉 饾悮饾惂饾悵 饾悢饾惉饾悮饾悹饾悶:
+鉃� /short: <b>饾棊饾棇饾柧 饾棈饾梺饾梻饾棇 饾柤饾棃饾梿饾梿饾柡饾棁饾柦 饾棎饾梻饾棈饾梺 饾棐饾棃饾棊饾棆 饾梾饾梻饾棁饾梽 饾棈饾棃 饾梹饾柧饾棈 饾棇饾梺饾棃饾棆饾棈饾柧饾柦 饾梾饾梻饾棁饾梽饾棇</b>
+鉃涴潠ゐ潡戰潠吼潡嗮潡夝潡咅潠�:
+<code>/short https://youtu.be/example...</code>"""
+
+    FUN_TXT ="""<b>G岽�岽嶀磭s</b> 
+    
+<b>鈿� 饾櫣饾殑饾殏饾殐 饾殏饾櫨饾櫦饾櫞 饾櫤饾櫢饾櫧饾櫝 饾櫨饾櫟 饾櫟饾殑饾櫧 饾殐饾櫡饾櫢饾櫧饾櫠'饾殏 鈿�</b>
+ 
+饾煟. /dice - 饾殎饾櫨饾櫥饾櫞 饾殐饾櫡饾櫞 饾櫝饾櫢饾櫜饾櫞 
+饾煠. /Throw 饾棃饾棆 /Dart - 饾殐饾櫨 饾櫦饾櫚饾櫤饾櫞 饾櫝饾櫚饾殎饾殐 
+3. /Runs - 饾殏饾櫨饾櫦饾櫞 饾殎饾櫚饾櫧饾櫝饾櫨饾櫦 饾櫝饾櫢饾櫚饾櫥饾櫨饾櫠饾殑饾櫞饾殏 
+4. /Goal or /Shoot - 饾殐饾櫨 饾櫦饾櫚饾櫤饾櫞 饾櫚 饾櫠饾櫨饾櫚饾櫥 饾櫨饾殎 饾殏饾櫡饾櫨饾櫨饾殐
+5. /luck or /cownd - 饾殏饾櫩饾櫢饾櫧 饾櫚饾櫧饾櫝 饾殐饾殎饾殘 饾殘饾櫨饾殑饾殎 饾櫥饾殑饾櫜饾櫤"""
+
+    ADMIN_TXT = """Help: <b>Admin mods</b>
+
+<b>NOTE:</b>
+This module only works for my admins
+
+<b>Commands and Usage:</b>
+鈥� /logs - <code>to get the rescent errors</code>
+鈥� /stats - <code>to get status of files in db.</code>
+鈥� /delete - <code>to delete a specific file from db.</code>
+鈥� /users - <code>to get list of my users and ids.</code>
+鈥� /chats - <code>to get list of the my chats and ids </code>
+鈥� /leave  - <code>to leave from a chat.</code>
+鈥� /disable  -  <code>do disable a chat.</code>
+鈥� /ban  - <code>to ban a user.</code>
+鈥� /unban  - <code>to unban a user.</code>
+鈥� /channel - <code>to get list of total connected channels</code>
+鈥� /broadcast - <code>to broadcast a message to all users</code>
+鈥� /grp_broadcast - <code>to broadcast a message to all groups</code>
+鈥� /gfilter - <code>group filter</code>
+鈥� /setskip - <code>skip no of files before indexing</code>"""
+    
+    STATUS_TXT = """<b>鈽� 饾殐饾櫨饾殐饾櫚饾櫥 饾櫟饾櫢饾櫥饾櫞饾殏: <code>{}</code>
+鈽� 饾殐饾櫨饾殐饾櫚饾櫥 饾殑饾殏饾櫞饾殎饾殏: <code>{}</code>
+鈽� 饾殐饾櫨饾殐饾櫚饾櫥 饾櫜饾櫡饾櫚饾殐饾殏: <code>{}</code>
+鈽� 饾殑饾殏饾櫞饾櫝 饾殏饾殐饾櫨饾殎饾櫚饾櫠饾櫞: <code>{}</code> 饾櫦饾殥饾櫛
+鈽� 饾櫟饾殎饾櫞饾櫞 饾殏饾殐饾櫨饾殎饾櫚饾櫠饾櫞: <code>{}</code> 饾櫦饾殥饾櫛</b>"""
+
+    CARB_TXT = """<b>Help</b> : 饾棖饾棓饾棩饾棔饾棦饾棥
+饾櫜饾櫚饾殎饾櫛饾櫨饾櫧 饾櫢饾殏 饾櫚 饾櫟饾櫞饾殑饾殐饾殑饾殎饾櫞 饾殐饾櫨 饾櫦饾櫚饾櫤饾櫞 饾殐饾櫡饾櫞 饾櫢饾櫦饾櫚饾櫠饾櫞 饾櫚饾殏 饾殏饾櫡饾櫨饾殕饾櫧 饾櫢饾櫧 饾殐饾櫡饾櫞 饾殐饾櫨饾櫩 饾殕饾櫢饾殐饾櫡 饾殘饾櫨饾殑饾殎饾櫞 饾殐饾櫞饾殗饾殐饾殏.
+饾櫟饾櫨饾殎 饾殑饾殏饾櫢饾櫧饾櫠 饾殐饾櫡饾櫞 饾櫦饾櫨饾櫝饾殑饾櫥饾櫞 饾櫣饾殑饾殏饾殐 饾殏饾櫞饾櫧饾櫝 饾殐饾櫡饾櫞 饾殐饾櫞饾殗饾殐 饾櫚饾櫧饾櫝 饾殎饾櫞饾櫩饾櫥饾殘 饾殐饾櫨 饾櫢饾殐 饾殕饾櫢饾殐饾櫡 /carbon 饾櫜饾櫨饾櫦饾櫦饾櫚饾櫧饾櫝 饾殐饾櫡饾櫞 饾櫛饾櫨饾殐 饾殕饾櫢饾櫥饾櫥 饾殎饾櫞饾櫩饾櫥饾殘 饾殕饾櫢饾殐饾櫡 饾殐饾櫡饾櫞 饾櫜饾櫚饾殎饾櫛饾櫨饾櫧 饾櫢饾櫦饾櫚饾櫠饾櫞"""
+
+
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+饾櫜饾殙饾櫝 饾櫛饾櫨饾殐饾殙
+"""
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Name - {}
+饾櫜饾殙饾櫝 饾櫛饾櫨饾殐饾殙
+"""
+    FILE_MSG = """
+<b>ʜᴇʟʟᴏ { }</b>
+ 
+<b><i>✅ ʏᴏᴜ ꜰɪʟᴇ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴜᴩʟᴏᴀᴅᴇᴅ ᴛᴏ ᴄʜᴀɴɴᴇʟ ᴩʟᴇᴀꜱᴇ ᴄʟɪᴄᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʙᴜᴛᴛᴏɴ,</i></b>
+
+<b>🗃 Fɪʟᴇ Nᴀᴍᴇ</b> <code>: { }</code>
+
+<b>🔖 Fɪʟᴇ Sɪᴢᴇ : { }</b>
+
+<b>⏱ ᴜᴩᴛɪᴍᴇ :</b> <code>{ }</code>
+"""
+    CHANNEL_CAP = """
+<b>🎟 ʀᴇqᴜᴇꜱᴛᴇᴅ вч : {query.from_user.first_name}</b>
+
+<code>{file_name}</code>
+
+<i>⚠️ ᴛʜɪꜱ ꜰɪʟᴇ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ꜰʀᴏᴍ ʜᴇʀᴇ ᴡɪᴛʜɪɴ 1 //Day// ᴀꜱ ɪᴛ ʜᴀꜱ ᴄᴏᴩyʀɪɢʜᴛ....</i>
+
+ വേറെ എവിടേലും ഫോർവേർഡ്  ചെയ്തിട്ട് ഡൗൺലോഡ് ചെയ്യുക ✊🏻️
+  """  
+    SUR_TXT = """
+<b>
+饾櫡饾櫞饾櫥饾櫥饾櫨 {},
+饾櫦饾殘 饾櫧饾櫚饾櫦饾櫞 饾櫢饾殏 <a href=https://t.me/{}>{}</a>, 饾櫢 饾櫜饾櫚饾櫧 饾櫩饾殎饾櫨饾殔饾櫢饾櫝饾櫞 饾櫦饾櫨饾殔饾櫢饾櫞饾殏, 饾櫣饾殑饾殏饾殐 饾櫚饾櫝饾櫝 饾櫦饾櫞 饾殐饾櫨 饾殘饾櫨饾殑饾殎 饾櫠饾殎饾櫨饾殑饾櫩 饾櫚饾櫧饾櫝 饾櫞饾櫧饾櫣饾櫨饾殘 
+</b>
+"""
+
+    IMDB_TEMPLATE_TXT = """
+<b>馃敄 岽浬礇薀岽� :<a href={url}>{title}</a>
+
+馃幁 散岽嚿词�岽噑 : {genres}
+馃帠 蕗岽�岽浬瓷� : <a href={url}/ratings>{rating}</a> / 10 (蕶岽�s岽囜磪 岽徤� {votes} 岽渟岽囀� 蕗岽�岽浬瓷�.)
+
+馃搯 蕪岽囜磤蕗 : {release_date}
+馃棡 薀岽�纱散岽溼磤散岽� : {languages}
+馃寧 岽勧磸岽溕瘁礇蕗蕪 : {countries}
+馃摉 s岽涐磸蕗蕪 : {plot} 
+
+漏{message.chat.title}</b>
+"""
+
+    CUSTOM_FILE_CAPTION = """<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <code>{file_name}</code>
+
+=========== • ✠ • ===========
+<b>▫️ ɢʀᴏᴜᴘ : @CinimaBranthen 
+▫️ ᴄʜᴀɴɴᴇʟ : @CbLinkzZ</b>
+=========== • ✠ • ===========</b>
+"""
+
+    RESTART_TXT = """
+<b>B岽忈礇 R岽噑岽涐磤蕗岽涐磭岽� !
+馃搮 D岽�岽涐磭 : <code>{}</code>
+鈴癟瑟岽嶀磭 : <code>{}</code>
+馃寪 T瑟岽嶀磭岽⑨磸纱岽� : <code>Asia/Kolkata</code>
+漏饾櫜饾殙饾櫝 饾櫛饾櫨饾殐饾殙</b>
+"""
+
+    MELCOW_ENG = """<b>H岽囀熓熱磸 {} 馃槏, A纱岽� W岽囀熱磩岽忈磵岽� T岽� {} G蕗岽忈礈岽� 鉂わ笍</b>"""
+
+    ALRT_TXT = """{ } മോനെ ഇത് നിനക്കുലതല്ല 🤭
+
+{ } ന്റെ റിക്വസ്റ്റ് ആണ് ഇത് 🙂
+
+Request your own 🥰
+
+© Cinimabranthen
+"""
+
+    OLD_ALRT_TXT = """അല്ലയോ മഹാൻ താങ്കൾ ക്ലിക്ക് ചെയ്തത് പഴയ മെസ്സേജ് ആണ് വേണമെങ്കിൽ ഒന്നും കൂടെ റിക്വസ്റ്റ് ചെയ് 😉
+
+You are using this for one of my old message, please send the request again
+"""
+    TOP_ALRT_MSG = """鈾伙笍 岽勈溼磭岽勧磱瑟纱散 隃吧熱磭 岽徤� 岽嵤� 岽呩磤岽涐磤蕶岽�隃贬磭... 鈾伙笍"""
+
+    MVE_NT_FND = """<b>饾殐饾櫡饾櫢饾殏 饾櫦饾櫨饾殔饾櫢饾櫞 I饾殏 饾櫧饾櫨饾殐 饾殘饾櫞饾殐 饾殎饾櫞饾櫥饾櫞饾櫚饾殏饾櫞饾櫝 饾櫨饾殎 饾櫚饾櫝饾櫝饾櫞饾櫝 饾殐饾櫨 饾櫝饾櫚饾殐饾殏饾櫛饾櫚饾殏饾櫞</b> """
+
+    NORSLTS = """鈽� #饾棥饾椉饾棩饾棽饾榾饾槀饾椆饾榿饾榾 鈽�
+饾棞饾棗 <b>: {}</b>
+饾棥饾棶饾椇饾棽 <b>: {}</b>
+饾棤饾棽饾榾饾榾饾棶饾棿饾棽 <b>: {}</b>
+漏饾櫜饾殙饾櫝 饾櫛饾櫨饾殐饾殙"""
+
+    I_CUDNT = """饾懐饾懍饾懗饾懗饾懚 {} 饾懓 饾應饾懚饾懠饾懗饾懌饾懙'饾懟 饾懎饾懓饾懙饾懌 饾懆饾懙饾拃 饾懘饾懚饾懡饾懓饾懍 饾懓饾懙 饾懟饾懐饾懆饾懟 饾懙饾懆饾懘饾懍 ."""
+
+    I_CUD_NT = """饾懐饾懍饾懗饾懗饾懚 {} 饾懓 饾應饾懚饾懠饾懗饾懌饾懙'饾懟 饾懎饾懓饾懙饾懌 饾懆饾懙饾拃饾懟饾懐饾懓饾懙饾懏 饾懝饾懍饾懗饾懆饾懟饾懍饾懌 饾懟饾懚 饾懟饾懐饾懆饾懟 . 饾應饾懐饾懍饾應饾懖 饾拃饾懚饾懠饾懝 饾懞饾懛饾懍饾懗饾懗饾懓饾懙饾懏."""
+    
+    CUDNT_FND = """饾懐饾懍饾懗饾懗饾懚 {} 饾懓 饾應饾懚饾懠饾懗饾懌饾懙'饾懟 饾懎饾懓饾懙饾懌 饾懆饾懙饾拃饾懟饾懐饾懓饾懙饾懏 饾懝饾懍饾懗饾懆饾懟饾懍饾懌 饾懟饾懚 饾懟饾懐饾懆饾懟 饾懌饾懓饾懌 饾拃饾懚饾懠 饾懘饾懍饾懆饾懙 饾懆饾懙饾拃 饾懚饾懙饾懍 饾懚饾懎 饾懟饾懐饾懍饾懞饾懍?"""
+    
+    REPRT_MSG = """ Reported To Admin"""
+    
+    SPELL_CHECK_ENG = """<i>Click on the button labeled Google, find the correct movie name and enter it here. But the movie / Tv. Web Series will get ..
+    
+If you still do not get it. Movie Name & year after @admin. Example: Add <b>@admin kala 2020</b> to the group in this way. The admin will upload within 24 hours
+    
+If you ask for a movie released in theaters, you will not get it, Movie is only available when ott Dvd is released</i>"""
+
+    SPELL_CHECK_MAL = """<i>Google, yendex എന്ന് എഴുതിയിരിക്കുന്ന ഏതെങ്കിലും ബട്ടണിൽ ക്ലിക്ക് ചെയ്ത് ശരിയായ സിനിമയുടെ പേര് കണ്ടെത്തി ഇവിടെ നൽകുക എന്നാലേ സിനിമ / Tv . Web സീരിയസ് കിട്ടുകയുള്ളു.. 
+
+എന്നിട്ടും കിട്ടുന്നില്ല എങ്കിൽ. @admin ശേഷം മൂവി Name & year. Example : <b>@admin kala 2020</b> ഈ രീതിയിൽ  ഗ്രൂപ്പിൽ സെന്റ് ചെയുക. 24 മണിക്കൂറിനുള്ളിൽ അഡ്മിൻ അപ്‌ലോഡ് ചെയ്യും
+
+തിയേറ്ററിൽ റിലീസ് ആയ മൂവിയാണ് ചോദിച്ചതെങ്കിൽ കിട്ടില്ല ott Dvd റിലീസ് ആയാൽ മാത്രമേ കിട്ടുള്ളൂ</i>"""
---
+++ info.py
@@ -0,0 +1,121 @@
+import re
+import os
+from os import environ
+from pyrogram import enums
+from Script import script
+
+import asyncio
+import json
+from collections import defaultdict
+from typing import Dict, List, Union
+from pyrogram import Client
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+class evamaria(Client):
+    filterstore: Dict[str, Dict[str, str]] = defaultdict(dict)
+    warndatastore: Dict[
+        str, Dict[str, Union[str, int, List[str]]]
+    ] = defaultdict(dict)
+    warnsettingsstore: Dict[str, str] = defaultdict(dict)
+
+    def __init__(self):
+        name = self.__class__.__name__.lower()
+        super().__init__(
+            ":memory:",
+            plugins=dict(root=f"{name}/plugins"),
+            workdir=TMP_DOWNLOAD_DIRECTORY,
+            api_id=APP_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            parse_mode=enums.ParseMode.HTML,
+            sleep_threshold=60
+        )
+
+# Bot information
+SESSION = environ.get('SESSION', 'Media_search')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+
+# Bot settings
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+PICS = (environ.get('PICS', 'https://telegra.ph/file/efbd273cbd0b215c9a4a3.jpg https://telegra.ph/file/acc6abf6d20d55150ce1a.jpg https://telegra.ph/file/c12a54051e1e7c09686b9.jpg https://telegra.ph/file/cfdc5a177c75b8a45c492.jpg https://telegra.ph/file/6499a76ed1cfb7796a5b7.jpg')).split()
+NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/1352172a6983f9279de16.jpg")
+SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/b66f3a65ce68e11e621cf.jpg")
+
+# Welcome area
+MELCOW_IMG = environ.get('MELCOW_IMG',"https://telegra.ph/file/f75dc5a5f440e2ffe43e7.jpg")
+MELCOW_VID = environ.get('MELCOW_VID',"https://telegra.ph/file/ede38b7090806dbbbde4c.mp4")
+
+
+
+# Admins, Channels & Users
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1908563535 1700221515 1837868455').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001303404632 -1001363669343 -1001413822114 -1001538014799 -1001548234832 -1001420818031 -1001434460585 -1001462830769 -1001231115988 -1001568183498 -1001552421125 -1001446511591 -1001496337557 -1001521379690 -1001455784876').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1908563535 1700221515 1837868455').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+auth_channel = environ.get('AUTH_CHANNEL','-1001709796292')
+auth_grp = environ.get('AUTH_GROUP','')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+
+# This is required for the plugins involving the file system.
+TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
+
+# Command
+COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
+
+# MongoDB information
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "doctorstrange")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+MONGO_URL = os.environ.get('MONGO_URL', "")
+
+#Downloader
+DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
+
+# Others
+DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
+PORT = os.environ.get("PORT", "8080")
+MAX_BTN = int(environ.get('MAX_BTN', "7"))
+S_GROUP = environ.get('S_GROUP',"https://t.me/czdbotz_support")
+MAIN_CHANNEL = environ.get('MAIN_CHANNEL',"https://t.me/czdbotz")
+FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/+BVP4yf8HHEFmNmJl")
+MSG_ALRT = environ.get('MSG_ALRT', '𝑇ℎ𝑎𝑛𝑘𝑄1�7 𝑇𝑜 𝐶𝑟𝑎𝑧𝑦 𝐷𝑒𝑣𝑒𝑙𝑜𝑝𝑒𝑟 💖')
+FILE_CHANNEL = int(environ.get('FILE_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'czdbotz_support')
+AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
+IMDB = is_enabled((environ.get('IMDB', "True")), True)
+SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CUSTOM_FILE_CAPTION}")
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
+LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
+SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
+PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
+PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+
+LOG_STR = "Current Cusomized Configurations are:-\n"
+LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
+LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
+LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n")
+LOG_STR += (f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n")
+LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_IMDB_DESCRIPTION is disabled , Plot will be shorter.\n")
+LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
+LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
+LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
