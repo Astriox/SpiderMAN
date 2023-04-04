fb8b749eb20512e446df9861fcd1319a49d73311
ᴀsᴛʀɪᴏx <71863236+Astriox@users.noreply.github.com>
2023-04-04 16:51:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -271,6 +271,7 @@ async def start(client, message: pyrogram.types.Message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption
+        reply_markup=InlineKeyboardMarkup(buttons),
         protect_content=True if pre == 'filep' else False,
         )
                     
